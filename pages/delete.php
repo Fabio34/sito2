2b79810add4422d938506d9a730c0761d437db7b
Fabio Roberti <robbyfabio96@gmail.com>
2017-06-29 01:32:23 +0200

    verisone 1.92

--- pages/delete.php
+++ pages/delete.php
@@ -6,7 +6,7 @@ $db = mysql_select_db('smartmuseum', $connect);
 $NumPassaporto = $_POST['NumPassaporto'];
 
 $query = sprintf("SELECT NumPassaporto FROM reperto WHERE '%u' = reperto.NumPassaporto",mysql_real_escape_string($NumPassaporto));
-$temp = mysql_query($query,$connect);
+$temp = mysqli_query($query,$connect);
 if(mysql_num_rows($temp) == 0) {	//ovvero nessun reperto con quel passaporto
 	$str2= "<script type='text/javascript'>alert('Numero passaporto non trovato!');</script>";
 	echo $str2;
@@ -14,14 +14,14 @@ if(mysql_num_rows($temp) == 0) {	//ovvero nessun reperto con quel passaporto
 }
 
 $query_destroy = sprintf( "delete from reperto where '%u' = reperto.NumPassaporto",mysql_real_escape_string($NumPassaporto));
-$res = mysql_query($query_destroy,$connect);
+$res = mysqli_query($query_destroy,$connect);
 
 //se ci sono file multimediali
 $query_file_multi = sprintf("SELECT * FROM multimedia WHERE '%u' = multimedia.NumPassaporto",mysql_real_escape_string($NumPassaporto));
 $query_file_multi_del = sprintf("DELETE FROM multimedia WHERE '%u' = multimedia.NumPassaporto",mysql_real_escape_string($NumPassaporto));
 
-if(mysql_num_rows( mysql_query($query_file_multi,$connect) ) > 0)
-	$multi_des = mysql_query($query_file_multi_del,$connect);
+if(mysql_num_rows( mysqli_query($query_file_multi,$connect) ) > 0)
+	$multi_des = mysqli_query($query_file_multi_del,$connect);
 
 mysql_close($connect);
 ?>
